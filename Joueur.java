2dc710347888239827453fd9696b50aa354fad6d
DokiMehia <so05.li.school@gmail.com>
2026-03-28 20:03:36 +0100

    Update Joueur.java
    Ajout fonctions estMortSoif et echangerEau

--- Joueur.java
+++ Joueur.java
@@ -46,4 +46,28 @@ public class Joueur {
     public void setNom(String name){
         this.nom = name;
     }
+
+    /**
+     * Verifie si le joueur est mort de soif
+     * @return true si le joueur est mort de soif
+     */
+    public boolean estMortSoif() {
+        return this.niveauEau == 0;
+    }
+
+    /**
+     * Donner l'eau de ce joueur au joueur j
+     * @param j le joueur qui recoit
+     * @return true si l'operation a pu se faire
+     */
+    public boolean echangerEau(Joueur j) {
+        if (this.niveauEau>0) {
+            if (j.niveauEau<2) {
+                j.niveauEau++;
+                this.niveauEau--;
+                return true;
+            }   
+        }
+        return false;
+    }
 }
